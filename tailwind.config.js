6dbe1f1611c0e70485a3b4ceed0068c3291d6890
Juan Romero <juan.romero.dev@gmail.com>
2022-05-19 21:32:05 -0400

    feat: linter changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+const tailwidConfig = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+module.exports = tailwidConfig
