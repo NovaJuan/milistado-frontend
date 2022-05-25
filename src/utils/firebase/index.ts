5f968ae781b0f68b762ac934f3225b740248b518
Juan Romero <juan.romero.dev@gmail.com>
2022-05-25 16:53:09 -0400

    feat: linting changes

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -1,8 +1,7 @@
+import * as config from '../../config'
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-
-import * as config from '../../config'
 
 const firebaseConfig = {
   apiKey: config.NEXT_PUBLIC_FIREBASE_API_KEY,
